51e8d80f931e3516f177d8ad907617edea2a0e21
Paula Suarez <pauladzuck@hotmail.com>
2018-06-26 09:02:18 -0500

    Adding updates to: readme, index and app.js files

---
+++ app.js
@@ -0,0 +1,15 @@
+//assign first and last name to variables
+var firstName= "Paula";
+var lastName = "Suarez";
+
+console.log (firstName);
+console.log (lastName);
+
+//assign value 50 for 50 states to a constaint
+const states = 50;
+console.log (states);
+ 
+//assign numbers and add 5 + 4
+var number1 = 5;
+var number2 = 4;
+console.log(number1+number2);
