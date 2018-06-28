d9ed708ea853aae570c98f30bf973a176d31ce65
Paula Suarez <pauladzuck@hotmail.com>
2018-06-28 13:53:30 -0500

    Added more tasks to javascript assignment

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 //assign first and last name to variables
-var firstName= "Paula";
+var firstName = "Paula";
 var lastName = "Suarez";
 
-console.log (firstName);
-console.log (lastName);
+console.log(firstName);
+console.log(lastName);
 
 //assign value 50 for 50 states to a constaint
 const states = 50;
-console.log (states);
- 
+console.log(states);
+
 //assign numbers and add 5 + 4
 var number1 = 5;
 var number2 = 4;
-console.log(number1+number2);
- 
+console.log(number1 + number2);
+
 //charCodeAt() comparison between first letter of my name to the letter "L"
 
 //finding ASCII value of letter P
@@ -36,8 +36,8 @@ if (pUnicode > lUnicode) {
 }
 
 //setting up my function from lecture example
-console.log(add(5,5));
-function add (num1, num2){
+console.log(add(5, 5));
+function add(num1, num2) {
     return num1 + num2;
 }
 
@@ -47,13 +47,70 @@ function sayHello() {
     alert("hello world");
 }
 
-
+//checkAge function that checks under 21--alert that they can't view page if 21 or older they can view page.
 console.log(checkAge("Paula", 20));
-function checkAge(name, age){
-    if (age <21){
-       alert ("Sorry name you aren't old enough to view this page!");
-    } 
-
-    
+function checkAge(name, age) {
+    if (age < 21) {
+        alert('Sorry ' + name + ' you are not old enough to view this page.');
+    } else {
+        alert('Congrats ' + name + ' you are old enough to view this page.');
+    }
 }
 
+//checkAge function that checks under 21--alert that they can't view page if 21 or older they can view page.
+console.log(checkAge("Charles", 21));
+function checkAge(name, age) {
+    if (age < 21) {
+        alert('Sorry ' + name + ' you are not old enough to view this page.');
+    } else {
+        alert('Congrats ' + name + ' you are old enough to view this page.');
+    }
+}
+
+//checkAge function that checks under 21--alert that they can't view page if 21 or older they can view page.
+console.log(checkAge("Abby", 27));
+function checkAge(name, age) {
+    if (age < 21) {
+        alert('Sorry ' + name + ' you are not old enough to view this page.');
+    } else {
+        alert('Congrats ' + name + ' you are old enough to view this page.');
+    }
+}
+
+//checkAge function that checks under 21--alert that they can't view page if 21 or older they can view page.
+console.log(checkAge("James", 18));
+function checkAge(name, age) {
+    if (age < 21) {
+        alert('Sorry ' + name + ' you are not old enough to view this page.');
+    } else {
+        alert('Congrats ' + name + ' you are old enough to view this page.');
+    }
+}
+
+//checkAge function that checks under 21--alert that they can't view page if 21 or older they can view page.
+console.log(checkAge("John", 17));
+function checkAge(name, age) {
+    if (age < 21) {
+        alert('Sorry ' + name + ' you are not old enough to view this page.');
+    } else {
+        alert('Congrats ' + name + ' you are old enough to view this page.');
+    }
+}
+//Array with fav veggies
+var veggies = ['green beans', 'tomatoes', 'lettuce'];
+//testing alert 
+//alert(veggies[1]);
+
+//Using same array with for each loop to display list in console
+veggies.forEach(function (veggies) {
+    console.log(veggies);
+});
+
+//Object 
+
+var people = {
+    name: 'Paula',
+    age: '41',
+};
+
+    console.log(people.name);
